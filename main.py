29bb67ccb2cd404cda17d3dd9ffd67c8c539c794
shinelin <shinelinxx@gmail.com>
2025-06-04 08:30:48 -0400

    feat(add): 新增消息类型

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from pkg.plugin.context import register, handler, llm_func, BasePlugin, APIHost, EventContext
 from pkg.plugin.events import *  # 导入事件类
-
+from pkg.platform.types import message as platform_message
+import json
 
 # 注册插件
-@register(name="Hello", description="hello world", version="0.1", author="RockChinQ")
-class HelloPlugin(BasePlugin):
+@register(name="Text2LangBotMsgChain", description="transfer text (eg. from dify) to message chain", version="0.1", author="shinelinxx")
+class Text2LangBotMsgChain(BasePlugin):
 
     # 插件加载时触发
     def __init__(self, host: APIHost):
@@ -14,35 +15,38 @@ class HelloPlugin(BasePlugin):
     async def initialize(self):
         pass
 
-    # 当收到个人消息时触发
-    @handler(PersonNormalMessageReceived)
-    async def person_normal_message_received(self, ctx: EventContext):
-        msg = ctx.event.text_message  # 这里的 event 即为 PersonNormalMessageReceived 的对象
-        if msg == "hello":  # 如果消息为hello
+    @handler(NormalMessageResponded)
+    async def on_normal_message_responded(self, ctx: EventContext):
+        """处理dify消息回复"""
+        # # 群id
+        # group_id = None \
+        #     if ctx.event.launcher_id == ctx.event.sender_id \
+        #         else ctx.event.launcher_id
+        # # 发送方id
+        # sender_id = ctx.event.sender_id
 
-            # 输出调试信息
-            self.ap.logger.debug("hello, {}".format(ctx.event.sender_id))
-
-            # 回复消息 "hello, <发送者id>!"
-            ctx.add_return("reply", ["hello, {}!".format(ctx.event.sender_id)])
-
-            # 阻止该事件默认行为（向接口获取回复）
-            ctx.prevent_default()
-
-    # 当收到群消息时触发
-    @handler(GroupNormalMessageReceived)
-    async def group_normal_message_received(self, ctx: EventContext):
-        msg = ctx.event.text_message  # 这里的 event 即为 GroupNormalMessageReceived 的对象
-        if msg == "hello":  # 如果消息为hello
-
-            # 输出调试信息
-            self.ap.logger.debug("hello, {}".format(ctx.event.sender_id))
-
-            # 回复消息 "hello, everyone!"
-            ctx.add_return("reply", ["hello, everyone!"])
-
-            # 阻止该事件默认行为（向接口获取回复）
-            ctx.prevent_default()
+        # 消息体
+        response_text = ctx.event.response_text
+        try:
+            message_elements = []
+            message_list = json.loads(response_text)
+            for item in message_list:
+                msg_type = item.get("type", "")
+                if msg_type == "WeChatAppMsg":
+                    message_elements.append(
+                        platform_message.WeChatAppMsg(app_msg = item.get("app_msg", "")))
+                if msg_type == "Image":
+                    message_elements.append(
+                        platform_message.Image(url = item.get("url", "")))
+                if msg_type == "Plain":
+                    message_elements.append(
+                        platform_message.Plain(text = item.get("text", "")))
+            if len(message_elements) > 0:    
+                await ctx.reply(platform_message.MessageChain(message_elements))
+                ctx.prevent_default()  # 阻断默认行为
+        except Exception as e:
+            self.ap.logger.error(f"回复消息处理异常: {str(e)}")
+    
 
     # 插件卸载时触发
     def __del__(self):
